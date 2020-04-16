d7a8284c0a682dc83431579309a7393d3413d217
Yang Sun <ysun62@ucsc.edu>
2020-04-16 16:29:54 -0700

    second commit

---
+++ client/build/precache-manifest.e443e18d1856859ab83fc018f8a1aa70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1437f0cef945f0cd77502af5b8df4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fed06ee1a1ae965ef4",
+    "url": "/static/css/main.46c32a2f.chunk.css"
+  },
+  {
+    "revision": "44cc729f931cca1526b5",
+    "url": "/static/js/2.691efcf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.691efcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fed06ee1a1ae965ef4",
+    "url": "/static/js/main.9620d5cb.chunk.js"
+  },
+  {
+    "revision": "a5581670c6fa053af376",
+    "url": "/static/js/runtime-main.71369147.js"
+  }
+]);
